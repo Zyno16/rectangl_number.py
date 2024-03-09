69d14327191d42a126eacbf5e56169c010a2ea55
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2024-03-09 07:32:50 +0000

    Add files via upload

---
+++ aptitude.py
@@ -0,0 +1,12 @@
+
+n =int(input("enter the number line"))
+c = int(input("enter the number cologne"))
+for i in range(1,n+1):
+    for j in range(1,c+1):
+        print(i,end ="")
+    print()
+
+
+
+ 
+
